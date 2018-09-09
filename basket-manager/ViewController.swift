294329315dcd5217a2750c34f7189b372609fee2
nobuhiroharada <nobuhiroharada@hotmail.com>
2018-09-10 06:33:20 +0900

    スコア+-追加

--- basket-manager/ViewController.swift
+++ basket-manager/ViewController.swift
@@ -10,16 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // スコアA
+    @IBOutlet weak var scoreATextField: UITextField!
+    @IBOutlet weak var scoreALabel: UILabel!
+    @IBOutlet weak var scoreAMinusBtn: UIButton!
+    @IBOutlet weak var scoreAPlusBtn: UIButton!
+    var scoreA: Int = 0
+    
+    // スコアB
+    @IBOutlet weak var scoreBTextField: UITextField!
+    @IBOutlet weak var scoreBLabel: UILabel!
+    @IBOutlet weak var scoreBMinusBtn: UIButton!
+    @IBOutlet weak var scoreBPlusBtn: UIButton!
+    var scoreB: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        scoreATextField.text = "Team A"
+        scoreBTextField.text = "Team B"
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: - チームAスコア Actions
+    @IBAction func scoreAMinusBtn(_ sender: UIButton) {
+        if scoreA > 0 {
+            scoreA -= 1
+            scoreALabel.text = String(scoreA)
+        }
     }
-
-
+    
+    @IBAction func scoreAPlusBtn(_ sender: UIButton) {
+        if scoreA < 1000 {
+            scoreA += 1
+            scoreALabel.text = String(scoreA)
+        }
+    }
+    
+    
+    
+    // MARK: - チームBスコア Actions
+    @IBAction func scoreBMinusBtn(_ sender: UIButton) {
+        if scoreB > 0 {
+            scoreB -= 1
+            scoreBLabel.text = String(scoreB)
+        }
+    }
+    
+    @IBAction func scoreBPlusBtn(_ sender: UIButton) {
+        if scoreB < 1000 {
+            scoreB += 1
+            scoreBLabel.text = String(scoreB)
+        }
+    }
 }
 
